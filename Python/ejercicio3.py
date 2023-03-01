770478799da90aee70ef991a6b32bb00ca70eaea
MartinDiaz2004 <maximiliano.6to.progra@gmail.com>
2023-03-01 12:06:25 -0600

    trabajo3

---
+++ Python/ejercicio3.py
@@ -0,0 +1,22 @@
+/pedir nombre , edad ,presio de su carro
+/calcular numero de tenensia
+ /(1.3%)
+
+ImpuestoTenencia = 0.013
+
+micadenadetexto ="hola"
+
+nombre = input ("Nombre: ")
+nombre = str (nombre)
+
+edad = input ("ingrese edad")
+edad = int(edad)
+
+PrecioCarro = input ("ingrese el pecio del carro")
+PrecioCarro = int (PrecioCarro)
+
+
+PrecioTenencia = PrecioCarro * ImpuestoTenencia
+print("Precio tenecia : " ,PrecioTenencia)
+
+
